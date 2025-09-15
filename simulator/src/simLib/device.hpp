24bd6393a3ca4e031ee7d453699a376dc1620e78
myn <myn.create@gmail.com>
2025-09-15 18:20:44 +0000

    Timing Simulator Initial Setup
      - A simple discrete timing simulator for estimating runtime
      - Initial setup of base device and a simple timing link class
      - c_SimpleLink is an optimized deque with FIFO policy

---
+++ simulator/src/simLib/device.hpp
@@ -0,0 +1,31 @@
+// base device class for all the simModules
+// implements the basic IO structures and functions
+// all devices should inherit from this class
+
+#ifndef SIMLIB_DEVICE_HPP
+#define SIMLIB_DEVICE_HPP
+
+#include<cstdint>
+
+class c_Device{
+  public:
+    c_Device(){
+        m_simClock = 0;
+    };
+    virtual ~c_Device(){};
+
+    virtual void init() = 0;
+    virtual void run()  = 0;
+    virtual void end()  = 0;
+    void tick() { m_simClock++; }
+    uint64_t simClock() { return m_simClock; }
+
+  protected:
+    uint8_t deviceID;
+    uint8_t deviceType;
+    uint64_t m_simClock;
+
+}
+
+
+#endif // SIMLIB_DEVICE_HPP
---
+++ simulator/src/simLib/simpleLink.hpp
@@ -0,0 +1,76 @@
+// simple link between two modules
+// every link is a queue with FIFO policy
+#ifndef SIMPLELINK_HPP
+#define SIMPLELINK_HPP
+#include<cstdint>
+
+template <typename T>
+class c_SimpleLink {
+  public:
+    c_SimpleLink(uint16_t x_size);
+    ~c_SimpleLink();
+
+    void canWrite();
+    T read();
+    void push_back(T x_packet);
+    T pop_front();
+    uint16_t size();
+    bool isFull();
+
+  private:
+    uint16_t m_size;
+    T* m_array;
+    uint16_t m_head;
+    uint16_t m_tail;
+};
+
+template <typename T>
+c_SimpleLink<T>::c_SimpleLink(uint16_t x_size) {
+    m_size = x_size;
+    m_array = new T[m_size];
+    m_head = 0;
+    m_tail = 0;
+}
+
+template <typename T>
+void
+canWrite() {
+    return ((m_size -  std::abs(m_head - m_tail)) != 0);
+}
+
+template <typename T>
+uint16_t
+size() { return m_size; }
+
+template <typename T>
+bool
+isFull() {return ((std::abs(m_head - m_tail) - m_size) == 0) }
+
+template <typaname T>
+T
+read() {
+    assert(m_array[m_head]);
+    return m_array[m_head];
+}
+
+template <typename T>
+T 
+pop_front() {
+    assert(m_array[m_head]);
+    T l_packet = m_array[m_head];
+    m_array[m_head] = T{0};
+    m_head = (m_head + 1) % m_size;
+    return l_packet;
+}
+
+template <typename T>
+void
+push_back(T x_packet) {
+    assert(canWrite());
+    assert(x_packet)
+    m_array[m_tail] = x_packet;
+    m_tail++;
+}
+
+
+#endif // SIMPLELINK_HPP
